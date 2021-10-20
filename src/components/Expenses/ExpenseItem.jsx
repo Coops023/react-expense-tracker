079907ceb6bcaa7730893bfb12aa0ae5a6fd579e
Coops023 <bjorkc27@gmail.com>
2021-10-20 15:54:01 +0200

    added form for adding new expenses

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import Card from "./Card";
+import { useState } from "react";
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.expenses.title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expenses.date} />
       <div className="expense-item__description">
-        <h2>{props.expenses.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.expenses.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
