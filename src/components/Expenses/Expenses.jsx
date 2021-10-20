a13fff0f5d6ddb7094182b32ea33dd558c5402e5
Coops023 <bjorkc27@gmail.com>
2021-10-20 16:47:33 +0200

    lifted state up from filter into expense

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 export default function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterhandler = (filtered) => {
+    setFilteredYear(filtered);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onFilter={filterhandler} />
       <ExpenseItem expenses={props.expenses[0]} />
       <ExpenseItem expenses={props.expenses[1]} />
       <ExpenseItem expenses={props.expenses[2]} />
